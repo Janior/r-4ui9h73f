eb7e11eb523091d797f811e83ecf3aa8446f1667
Janior <janior_orlando@hotmail.com>
2018-12-21 04:42:50 -0800

    Inicio Temas React y Redux Retos Repetidor

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+   state = { 
+     text:"" 
+   } 
+   handleInput = this.handleInput.bind(this); 
+   handleInput (e) { 
+    this.setState({ 
+      text: e.target.value 
+    }) 
+   } 
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handleInput}/> 
+         <p className="repeater">{this.state.text}</p> 
       </div>
+
     );
   }
 }
